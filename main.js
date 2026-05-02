e0ed5878fe44707ef437bb1bb0bc8559be4eaa7b
Deuxx2025 <cristhianloa15@gmail.com>
2026-05-02 22:36:56 +0100

    Get twitch token function for the server

---
+++ main.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const axios = require('axios');
+
+async function getTwitchToken() {
+    const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
+        params: {
+            client_id: process.env.TWITCH_CLIENT_ID,
+            client_secret: process.env.TWITCH_CLIENT_SECRET,
+            grant_type: 'client_credentials'
+        }
+    });
+    return response.data.access_token; 
+}
