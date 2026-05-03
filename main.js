b0e8dd9aabf585d281cdd283f4da6bb35eddf532
Deuxx2025 <cristhianloa15@gmail.com>
2026-05-03 18:26:25 +0100

    Multiplier and interval functions

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ dotenv.config();
 const express = require('express');
 const axios = require('axios');
 
+let pointsPool = 0;
+let intervalID;
+
 async function getTwitchToken() {
     const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
         params: {
@@ -13,3 +16,27 @@ async function getTwitchToken() {
     });
     return response.data.access_token; 
 }
+
+async function getViewerCount() {
+    return 5 
+}
+
+async function viewerMultiplier(){
+    const viewers = await getViewerCount();
+    return 1 + Math.log(viewers) * 0.5;
+}
+
+async function tick(){
+    const multiplier = await viewerMultiplier();
+    //change this at your will, it can be `pointsPool += yourVariable * multiplier`
+    pointsPool += multiplier;
+    console.log(`Points: ${pointsPool} | Multiplier: ${multiplier}`);
+}
+
+function startInterval (){
+    if (intervalID){
+    clearInterval(intervalID);
+    }
+    intervalID = setInterval(tick, 60000)
+}
+startInterval()
